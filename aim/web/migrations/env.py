ffb476806c1fe621de0e5a2a846c4f1712071b6d
Dongki Lee <waitlee@pubg.com>
2022-07-28 11:47:08 +0900

    plus

--- aim/web/migrations/env.py
+++ aim/web/migrations/env.py
@@ -92,6 +92,8 @@ def run_migrations_online():
             context.run_migrations()
 
 if context.is_offline_mode():
-    run_migrations_offline()
+    # run_migrations_offline()
+    ...
 else:
-    run_migrations_online()
+    # run_migrations_online()
+    ...
